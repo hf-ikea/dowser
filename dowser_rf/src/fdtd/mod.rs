d0a932d7e7b1d71d2e07a9d85482710bea8a353d
Giovanni Forma <bluehairguyiscool@gmail.com>
2024-12-16 18:29:45 -0800

    ttest

--- dowser_rf/src/fdtd/mod.rs
+++ dowser_rf/src/fdtd/mod.rs
@@ -1,16 +1,16 @@
 use std::ops::{Index, IndexMut};
 
-trait EMField {
+trait Field {
     type Index;
     fn get(&self, idx: Self::Index) -> &f64;
     fn get_mut(&mut self, idx: Self::Index) -> &mut f64;
     /// Returns the largest index in every dimension that exists in this array
     fn max_index(&self) -> Self::Index;
 }
-pub struct OneDEMField {
+pub struct OneDField {
     array: Box<[f64]>,
 }
-impl OneDEMField {
+impl OneDField {
     pub fn new_zeroed(size: usize) -> Self {
         Self {
             array: vec![0.0; size].into_boxed_slice(),
@@ -23,7 +23,7 @@ impl OneDEMField {
     }
 }
 
-impl EMField for OneDEMField {
+impl Field for OneDField {
     type Index = usize;
 
     fn get(&self, idx: Self::Index) -> &f64 {
@@ -38,7 +38,7 @@ impl EMField for OneDEMField {
     }
 }
 
-impl Index<usize> for OneDEMField {
+impl Index<usize> for OneDField {
     type Output = f64;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -46,25 +46,36 @@ impl Index<usize> for OneDEMField {
     }
 }
 
-impl IndexMut<usize> for OneDEMField {
+impl IndexMut<usize> for OneDField {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.array[index]
     }
 }
 #[cfg(test)]
 mod tests {
-    use super::{EMField, OneDEMField};
+    use crate::consts::FREE_SPACE_IMPEDANCE;
+
+    use super::OneDField;
 
     #[test]
     fn fdtd_1d() {
-        let mut zeroed_field = OneDEMField::new_zeroed(10);
-        let initialized_field = OneDEMField::from_initial_state(&[0.0, 1.0, 0.0]);
-        // write to index 2
-        zeroed_field[2] = 0.0;
+        const SIZE: usize = 200;
+        let mut e_field: OneDField = OneDField::new_zeroed(SIZE);
+        let mut h_field: OneDField = OneDField::new_zeroed(SIZE);
+        let max_time: usize = 250;
 
-        let mut m: f64 = 0.0;
-        let mut q: f64 = 0.0;
-        let delta_x: f64 = 0.001;
-        let delta_t: f64 = 0.001;
+        for q in 0..max_time {
+            for m in 0..(SIZE - 1) {
+                h_field[m] = h_field[m] + (e_field[m + 1] - e_field[m]) / FREE_SPACE_IMPEDANCE;
+            }
+
+            for m in 1..SIZE {
+                e_field[m] = e_field[m] + (h_field[m] - h_field[m - 1]) * FREE_SPACE_IMPEDANCE;
+            }
+
+            e_field[0] = (-(q as f64 - 30.0) * (q as f64 - 30.0) / 100.0).exp();
+
+            println!("{0}", e_field[50]);
+        }
     }
 }
